136a2e351163e4ebbcfa1b3c70bae4efc7e12c8b
Marcos Garcia <marcos.x.garcia.-nd@disney.com>
2016-01-30 23:28:41 -0800

    Initial Commit

---
+++ unzipFiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  unzipFiles
+//
+//  Created by marcosgn1 on 1/30/16.
+//  Copyright © 2016 marcosgn1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ unzipFiles/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  unzipFiles
+//
+//  Created by marcosgn1 on 1/30/16.
+//  Copyright © 2016 marcosgn1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
